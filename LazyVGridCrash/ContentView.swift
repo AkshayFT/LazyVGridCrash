d47ffa320dbc1c5fca6e30d6481a90ca3acdeffd
Akshay <akshay@fluidtouch.biz>
2023-11-22 19:15:56 +0530

    LazyVGrid Crash demonstration added

--- LazyVGridCrash/ContentView.swift
+++ LazyVGridCrash/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    let layout = Array(repeating: GridItem(.fixed(200)), count: 3)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.vertical) {
+            LazyVGrid(columns: layout) {
+                ForEach(0..<30) { _ in
+                    Color.blue
+                        .frame(height: 200)
+                }
+            }
         }
-        .padding()
     }
 }
 
