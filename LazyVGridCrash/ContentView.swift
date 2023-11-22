0f61075ecf0ae6c05cea5a519fd92e8af263ea0f
Akshay <akshay@fluidtouch.biz>
2023-11-22 19:08:42 +0530

    Initial Commit

---
+++ LazyVGridCrash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LazyVGridCrash
+//
+//  Created by Akshay on 22/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LazyVGridCrash/LazyVGridCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridCrashApp.swift
+//  LazyVGridCrash
+//
+//  Created by Akshay on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
